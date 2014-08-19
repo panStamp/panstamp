719ad36c7776312751b7e83ced72ba33a594e54a
dberenguer <dberenguer@usapiens.com>
2014-08-19 15:55:43 +0200

    Added signature in EEPROM/INFOMEM

--- arduino-1.5/arduino-1.5_patch/libraries/swap/nvolat.h
+++ arduino-1.5/arduino-1.5_patch/libraries/swap/nvolat.h
@@ -31,12 +31,19 @@
  * EEPROM/Infomem addresses
  */
 #define NVOLAT_FREQ_CHANNEL       0x00   // 1-byte register
-#define NVOLAT_CARRIER_FREQ       0x01   // 1-byte register
+#define NVOLAT_NOT_USED           0x01   // 1-byte register
 #define NVOLAT_SYNC_WORD          0x02   // 2-byte register
 #define NVOLAT_DEVICE_ADDR        0x04   // 1 or 2 byte register
 #define NVOLAT_TX_INTERVAL        0x06   // 2-byte register
+#define NVOLAT_SIGNATURE          0x08   // 2-byte register
+#define NVOLAT_CARRIER_FREQ       0x0A   // 3-byte register
 #define NVOLAT_FIRST_CUSTOM       0x20
 
+// Signature
+#define NVOLAT_SIGNATURE_HIGH     0xAB
+#define NVOLAT_SIGNATURE_LOW      0xCD
+
+// Extended address
 #ifdef SWAP_EXTENDED_ADDRESS
 #define NVOLAT_DEVICE_ADDRESS     NVOLAT_DEVICE_ADDR
 #else
