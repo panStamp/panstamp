d19c41ebcf1b44b8f738cb5ba7e45933e044c98d
dberenguer <dberenguer@usapiens.com>
2014-11-19 20:07:20 +0100

    Removed use of Hardwareserial

--- arduino-1.5/sketches/ntc/ntc.ino
+++ arduino-1.5/sketches/ntc/ntc.ino
@@ -43,7 +43,7 @@
 
 
 // Digital output used to power the thermistor circuit
-#define NTC_POWER_PIN         14
+#define NTC_POWER_PIN         10
 // Analog pin used to read the NTC
 #define NTC_PIN               A1
 // Macros
@@ -61,7 +61,6 @@ THERMISTOR thermistor(NTC_PIN,        // Analog pin
  *
  * Arduino setup function
  */
- #include "HardwareSerial.h"
 void setup()
 {
   int i;
