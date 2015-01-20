fcf50859d3f552dd8a957be05ca6c3c8e6263fba
Daniel Berenguer <dberenguer@usapiens.com>
2015-01-20 22:30:43 +0100

    app folder now copied from the installer

--- arduino-1.5/arduino-1.5_patch/install.bat
+++ arduino-1.5/arduino-1.5_patch/install.bat
@@ -5,6 +5,7 @@ REM Install panStamp files for Arduino-1.5
 echo Installing patch...
 XCOPY libraries ..\libraries\ /s
 XCOPY hardware ..\hardware\ /s
+XCOPY app ..\app\ /s
 XCOPY win\platform.txt ..\hardware\panstamp\msp430\ /s
 XCOPY win\tools ..\hardware\tools\ /s
 echo Installation completed
